720b7f35c2d7ad143dd909304dc450b3133a79f6
citatydusi-sys <citatydusi@gmail.com>
2026-01-16 01:34:29 +0500

    Add debug output to build script

--- build.sh
+++ build.sh
@@ -2,14 +2,22 @@
 # Exit on error
 set -o errexit
 
+echo "Starting build process..."
+
 # Install dependencies
+echo "Installing dependencies..."
 pip install -r requirements.txt
 
 # Run migrations
+echo "Running migrations..."
 python manage.py migrate --no-input
 
 # Import data
+echo "Importing quiz data..."
 python manage.py import_data
 
 # Collect static files
+echo "Collecting static files..."
 python manage.py collectstatic --no-input --clear
+
+echo "Build completed successfully!"
